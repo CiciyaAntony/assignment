8938c8a84cbe2b0387b0f92d8e16eb6b13a8a9fd
CiciyaAntony <ciciya@qburst.com>
2018-08-09 11:01:47 +0530

    required changes

--- thirdassignment/src/productlist.js
+++ thirdassignment/src/productlist.js
@@ -1,30 +1,21 @@
 import React,{Component} from 'react';
 
 export default class Productlist extends Component{
-
-  render(){
- let id =0;
-    const products = this.props.products;
-    //console.log(products);
-  var name;
-
-    if(products.inStock)
-    {
+   render(){
+      const products = this.props.products;
+      var name;
+      if(products.inStock)
+      {
        name=products.name;
-
-    }
-    else
+      }
+      else
       {
        name=<span style ={{color:'red'}} >{ products.name}</span>
       }
-
-    return(
-
-
-
-<tr>
-    <td key={id++}>{name}</td>
-    <td key={id++}>{products.price}</td>
+      return(
+      <tr>
+    <td >{name}</td>
+    <td >{products.price}</td>
   </tr>
 
 
